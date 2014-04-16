185231054ab25ee918d35754f2667935752d44e8
Steve Clifton <steve@steveclifton.com>
2014-04-16 21:04:29 +0100

    - Adding progress dialogs while searching Twitter/Sentiments.  - Disable search button if no text present

--- AndroidApp/src/com/steveclifton/sentimentaltwitterandroid/activity/MainActivity.java
+++ AndroidApp/src/com/steveclifton/sentimentaltwitterandroid/activity/MainActivity.java
@@ -12,12 +12,15 @@ import com.steveclifton.sentimentaltwitterandroid.network.TwitterManager.QueryRe
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements QueryResponseListener, SentimentListener {
 	
@@ -27,10 +30,20 @@ public class MainActivity extends Activity implements QueryResponseListener, Sen
 	ArrayList<Tweet> mTweets = new ArrayList<Tweet>();
 	private Handler mHandler = new Handler();
 	
+	private Button mButton = null;
+	private EditText mEdit = null;
+	
+	private ProgressDialog mProgress = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mEdit = (EditText) findViewById(R.id.edit_searchterm);
+		mEdit.addTextChangedListener( mTextWatcher );
+		mButton = (Button) findViewById(R.id.button_search);
+		mButton.setEnabled(false);
 		
 		mTm = new TwitterManager();
 		mSentiment = new SentimentManager();
@@ -44,6 +57,12 @@ public class MainActivity extends Activity implements QueryResponseListener, Sen
 	}
 	
 	public void onSearch(View aView) {
+		mProgress = new ProgressDialog(this);
+		mProgress.setIndeterminate(true);
+		mProgress.setMessage(getString(R.string.search_tweets_msg));
+		mProgress.show();
+				
+		
 		EditText et = (EditText)findViewById(R.id.edit_searchterm);
 		String string = et.getText().toString();
 		mTm.startQuery(string, this);
@@ -54,20 +73,53 @@ public class MainActivity extends Activity implements QueryResponseListener, Sen
 	@Override
 	public void queryResponse(String aQuery, ArrayList<Tweet> aTweets) {
 		mTweets = aTweets;
+		mProgress.dismiss();
+		mProgress = null;
+		
+		mProgress = new ProgressDialog(this);
+		mProgress.setIndeterminate(false);
+		mProgress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		mProgress.setProgress(0);
+		mProgress.setMessage(getString(R.string.sentiment_tweets_msg));
+		mProgress.show();
+		
+		
 		mSentiment.checkSentiment(aTweets, this);
 	}
 
 	@Override
-	public void sentimentProgress(int aDone, int aTotal) {
+	public void sentimentProgress(int aDone, int aTotal) {		
+		float pc = ((float)aDone/(float)aTotal)*100.0f;
 		
-		// TODO - Replace with dialog	
-		Log.d("SJC","Sentiment "+aDone+"/"+aTotal);
+		mProgress.setProgress((int) pc);
+		
 		if ( aDone == aTotal ) {
+			mProgress.dismiss();
+			mProgress = null;
+			
 			Intent intent = new Intent();
 			intent.setClass(this, ListActivity.class);
 			intent.putExtra(ListActivity.TWEETS, mTweets);
 			startActivity(intent);	
 		}
-
 	}
+	
+	TextWatcher mTextWatcher = new TextWatcher() {
+		
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before, int count) {
+		}
+		
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+		}
+		
+		@Override
+		public void afterTextChanged(Editable s) {
+			// Disable button if no text to search for
+			mButton.setEnabled(s.toString().length() > 0);
+		}
+	};
+	
+	
 }
--- README.md
+++ README.md
@@ -3,5 +3,6 @@ SentimentalTwitterAndroid
 
  * Tweets searched for are limited to English, as the Sentiment search language is restricted to English too.
  * Only up to 10 tweets are requested, as Sentiment API only allows 500 "free" queries per day, after that they start charging.
-   This value can be changed in ****
- * The MaShape key needs to be specified, and is located in ***
+   This value can be changed in strings.xml (max_tweets)
+ * The MaShape key needs to be specified, and is located in "strings.xml" (mashape_key)
+   If no key specified, a Toast message is shown when searching, and all faces are Indifferent
